8cedd45026c6da4b5c334b52c8485cfe1ba7a0f8
xiaoheng <732722834@qq.com>
2025-04-23 22:21:59 +0800

    feat: part8 Dynamic configuration switch operating

--- group-buying-app/src/main/java/com/chuppch/config/RedisClientConfig.java
+++ group-buying-app/src/main/java/com/chuppch/config/RedisClientConfig.java
@@ -28,12 +28,10 @@ import java.io.IOException;
  */
 @Configuration
 @EnableConfigurationProperties(RedisClientConfigProperties.class)
-@Slf4j //TODO
 public class RedisClientConfig {
 
     @Bean("redissonClient")
     public RedissonClient redissonClient(ConfigurableApplicationContext applicationContext, RedisClientConfigProperties properties) {
-        log.info("初始化 RedissonClient，连接 Redis: {}:{}", properties.getHost(), properties.getPort()); //TODO
         Config config = new Config();
         // 根据需要可以设定编解码器；https://github.com/redisson/redisson/wiki/4.-%E6%95%B0%E6%8D%AE%E5%BA%8F%E5%88%97%E5%8C%96
         config.setCodec(JsonJacksonCodec.INSTANCE);
